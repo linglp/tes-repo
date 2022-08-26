4af430364d8efc4db475c98b44539f49e83230c0
linglp <linglp@umich.edu>
2022-08-26 14:46:58 -0400

    update extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import myst_parser
 
 # -- Project information -----------------------------------------------------
 
@@ -28,16 +27,7 @@ author = 'lingling'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.napoleon",
+    "sphinxcontrib.mermaid",
     'myst_parser'
 ]
 
